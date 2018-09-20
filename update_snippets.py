0b674dd70874b1b8b6c1d19423625b15cdd242f7
Rhilip <rhilipruan@gmail.com>
2018-09-20 19:03:56 +0800

    :sparkles: Upload SSdirectors file

--- update_snippets.py
+++ update_snippets.py
@@ -40,19 +40,42 @@ def get_dbid_from_name(name, year):
         return all_subject[0].get("id")
 
 
+def get_old_data_list(filename, rowname):
+    rank_file = os.path.join('.', 'data', filename)
+    f_csv = csv.DictReader(open(rank_file, encoding='utf-8'))
+    return [(row[rowname], row['dbid']) for row in f_csv]
+
+
+def get_list_raw(link, selector) -> list:
+    top_req = requests.get(link, headers=headers)
+    top_bs = BeautifulSoup(top_req.text, 'lxml')
+    return top_bs.select(selector)
+
+
+def write_data_list(filename, header, data):
+    rank_file = os.path.join('.', 'data', filename)
+    with open(rank_file, 'w', encoding='utf-8') as f:
+        f_csv = csv.DictWriter(f, header, dialect=csv.unix_dialect)
+        f_csv.writeheader()
+        f_csv.writerows(data)
+
+
+def write_snippets_json(filename, data):
+    snippets_json_file = os.path.join('.', 'snippets', filename)
+    data["update"] = int(time.time())
+    with open(snippets_json_file, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=3, sort_keys=True, ensure_ascii=False)
+
+
 def update_imdb_top_250():
     # Read our old data file
-    rank_file = os.path.join('.', 'data', 'IMDbtop250.csv')
-    f_csv = csv.DictReader(open(rank_file, encoding='utf-8'))
-    old_rank_list = [(row['imdbid'], row['dbid']) for row in f_csv]
+    old_rank_list = get_old_data_list("IMDbtop250.csv", "imdbid")
 
     # Get new top 250 rank list and update data file
-    imdb_top_list = []
-    imdb_top_req = requests.get('https://www.imdb.com/chart/top', headers=headers)
-    imdb_top_bs = BeautifulSoup(imdb_top_req.text, 'lxml')
-    imdb_top_list_raw = imdb_top_bs.select('table[data-caller-name="chart-top250movie"] > tbody > tr > td.titleColumn')
-
-    for item in imdb_top_list_raw:
+    top_list = []
+    top_list_raw = get_list_raw('https://www.imdb.com/chart/top',
+                                'table[data-caller-name="chart-top250movie"] > tbody > tr > td.titleColumn')
+    for item in top_list_raw:
         item_text = item.get_text(strip=True)
         item_search = re.search('(\d+)\.(.+?)\((\d+)\)', item_text)
         item_rank = item_search.group(1)
@@ -66,41 +89,29 @@ def update_imdb_top_250():
         else:
             item_dbid = get_dbid_from_imdbid(item_imdbid)
 
-        imdb_top_list.append(
+        top_list.append(
             {"rank": item_rank, "title": item_title, 'year': item_year, "imdbid": item_imdbid, "dbid": item_dbid})
 
-    # Save IMDb top 250 rank list for next time used
-    with open(rank_file, 'w', encoding='utf-8') as f:
-        f_csv = csv.DictWriter(f, ['rank', 'title', 'year', 'imdbid', 'dbid'], dialect=csv.unix_dialect)
-        f_csv.writeheader()
-        f_csv.writerows(imdb_top_list)
-
-    # Update snippets file
-    snippets_json_file = os.path.join('.', 'snippets', '01_IMDbtop250.json')
-    with open(snippets_json_file, 'w', encoding='utf-8') as f:
-        json.dump({
-            "title": "IMDb Top 250",
-            "short_title": "IMDb Top 250",
-            "href": "https://www.imdb.com/chart/top",
-            "top": 1,
-            "list": {str(i["dbid"]): i['rank'] for i in imdb_top_list},
-            "update": int(time.time())
-        }, f, indent=3, sort_keys=True)
+    # Write Data list and snippets file
+    write_data_list("IMDbtop250.csv", ['rank', 'title', 'year', 'imdbid', 'dbid'], top_list)
+    write_snippets_json('01_IMDbtop250.json', {
+        "title": "IMDb Top 250",
+        "short_title": "IMDb Top 250",
+        "href": "https://www.imdb.com/chart/top",
+        "top": 1,
+        "list": {str(i["dbid"]): i['rank'] for i in top_list},
+    })
 
 
 def update_afi_top_100():
     # Read our old data file
-    rank_file = os.path.join('.', 'data', 'AFilist.csv')
-    f_csv = csv.DictReader(open(rank_file, encoding='utf-8'))
-    old_rank_list = [(row['afiid'], row['dbid']) for row in f_csv]
+    old_rank_list = get_old_data_list("AFilist.csv", "afiid")
 
     # Get top 100 afilist and update data file
-    afi_top_list = []
-    afi_top_req = requests.get("http://www.afi.com/100years/movies10.aspx", headers=headers)
-    afi_top_bs = BeautifulSoup(afi_top_req.text, 'lxml')
-    afi_top_list_raw = afi_top_bs.select("#subcontent > div.pollListWrapper > div.pollList > div.listItemWrapper")
-
-    for item in afi_top_list_raw:
+    top_list = []
+    top_list_raw = get_list_raw("http://www.afi.com/100years/movies10.aspx",
+                                "#subcontent > div.pollListWrapper > div.pollList > div.listItemWrapper")
+    for item in top_list_raw:
         item_text = item.get_text(strip=True)
         item_search = re.search('(\d+)\.(.+?)\((\d+)\)', item_text)
         item_rank = item_search.group(1)
@@ -116,28 +127,84 @@ def update_afi_top_100():
         else:
             item_dbid = get_dbid_from_name(item_title, item_year)
 
-        afi_top_list.append(
+        top_list.append(
             {"rank": item_rank, "title": item_title, 'year': item_year, "afiid": item_afiid, "dbid": item_dbid})
 
-        # Save AFI top 100 rank list for next time used
-    with open(rank_file, 'w', encoding='utf-8') as f:
-        f_csv = csv.DictWriter(f, ['rank', 'title', 'year', 'afiid', 'dbid'], dialect=csv.unix_dialect)
-        f_csv.writeheader()
-        f_csv.writerows(afi_top_list)
+    # Write Data list and snippets file
+    write_data_list("AFilist.csv", ['rank', 'title', 'year', 'afiid', 'dbid'], top_list)
+    write_snippets_json('02_AFIlist.json', {
+        "title": "美国电影学会（AFI）“百年百大”排行榜",
+        "short_title": "AFI Top 100",
+        "href": "http://www.afi.com/100years/movies10.aspx",
+        "top": 2,
+        "list": {str(i["dbid"]): i['rank'] for i in top_list},
+    })
 
-    # Update snippets file
-    snippets_json_file = os.path.join('.', 'snippets', '02_AFIlist.json')
-    with open(snippets_json_file, 'w', encoding='utf-8') as f:
-        json.dump({
-            "title": "美国电影学会（AFI）“百年百大”排行榜",
-            "short_title": "AFI Top 100",
-            "href": "http://www.afi.com/100years/movies10.aspx",
-            "top": 2,
-            "list": {str(i["dbid"]): i['rank'] for i in afi_top_list},
-            "update": int(time.time())
-        }, f, indent=3, sort_keys=True, ensure_ascii=False)
+
+def update_cclist():
+    pass
+
+
+def update_ss_(csvfile, jsonfile, reqlink, basedict):
+    # Read our old data file
+    old_rank_list = get_old_data_list(csvfile, "bfid")
+
+    # Get rank list and update data file
+    top_list = []
+    top_list_raw = get_list_raw(reqlink,
+                                "#region-content-left-1 > div > div > div > div > div > div:nth-of-type(2) > div:nth-of-type(2) > h3")
+    for item in top_list_raw:
+        item_text = item.get_text(" ", strip=True)
+        print(item_text)
+        if re.search('Histoire', item_text):
+            item_search = re.search('(\d+) (.+)', item_text)
+            item_year = 1989
+            item_bfid = "Histoire"
+        else:
+            item_search = re.search('(\d+) (.+?)\((\d{4})\)', item_text)
+            item_year = int(item_search.group(3))
+            item_bfid = re.search("films-tv-people/(.+)$", item.find("a")["href"]).group(1)
+        item_rank = item_search.group(1)
+        item_title = item_search.group(2).strip()
+
+        item_dbid_search = list(filter(lambda x: x[0] == item_bfid, old_rank_list))
+        if len(item_dbid_search) > 0:  # Use old
+            item_dbid = item_dbid_search[0][1]
+        else:
+            item_dbid = get_dbid_from_name(item_title, item_year)
+
+        top_list.append(
+            {"rank": item_rank, "title": item_title, 'year': item_year, "bfid": item_bfid, "dbid": item_dbid})
+
+    # Write Data list and snippets file
+    write_data_list(csvfile, ['rank', 'title', 'year', 'bfid', 'dbid'], top_list)
+    basedict["list"] = {str(i["dbid"]): i['rank'] for i in top_list}
+    write_snippets_json(jsonfile, basedict)
+
+
+def update_sscritics():
+    update_ss_("SScritics.csv", '04_SScritics.json',
+               "https://www.bfi.org.uk/films-tv-people/sightandsoundpoll2012/critics", {
+                   "title": "《视与听》影史最佳影片-影评人Top100",
+                   "short_title": "视与听影评人百佳",
+                   "href": "https://www.bfi.org.uk/films-tv-people/sightandsoundpoll2012/critics",
+                   "top": 4,
+               })
+
+
+def update_ssdirectors():
+    update_ss_("SSdirectors.csv", '05_SSdirectors.json',
+               "https://www.bfi.org.uk/films-tv-people/sightandsoundpoll2012/directors", {
+                   "title": "《视与听》影史最佳影片-导演Top100",
+                   "short_title": "视与听导演百佳",
+                   "href": "https://www.bfi.org.uk/films-tv-people/sightandsoundpoll2012/directors",
+                   "top": 5,
+               })
 
 
 if __name__ == '__main__':
     update_imdb_top_250()
     update_afi_top_100()
+    update_cclist()
+    update_sscritics()
+    update_ssdirectors()

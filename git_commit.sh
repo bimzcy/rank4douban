b9af29840f7e648bcd3f467f52fb90ff1639f0e1
root <root@vultr.guest>
2018-10-12 23:32:57 +0800

    Daily update 2018-10-12

--- git_commit.sh
+++ git_commit.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
 
+SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+
+cd $SHELL_FOLDER
+python3 update_snippets.py
+python3 build_data.py
+
+DATE=`date +%Y-%m-%d`
+git commit -a -m "Daily update $DATE"
+git pull
+

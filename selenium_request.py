7f702d9fa47c84d26aaccfea5c56a329e579b545
Rhilip <rhilipruan@gmail.com>
2026-03-27 20:46:44 +0800

    fix: Update Selenium setup for GitHub Actions compatibility

--- selenium_request.py
+++ selenium_request.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
+from webdriver_manager.core.os_manager import ChromeType
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
